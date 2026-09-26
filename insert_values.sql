8f27d66cc172fffb811fe245607c45a985aaccbd
Aditya Singh <as1671109@gmail.com>
2026-09-26 11:08:17 +0530

    done with insert value

---
+++ insert_values.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+    customers (id, first_name, country, score) 
+    VALUES 
+    (6, 'Anna','USA',NULL),
+    (7, 'Sam',NULL,100),
+    (9, 'Andreas','Germany',NULL)
+
+SELECT * 
+FROM customers;
